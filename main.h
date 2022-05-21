a21e1f494cc906d8e4100c8bf9880a35e71b73d4
Juliusbruce <brucejulius048@gmail.com>
2022-05-21 14:02:09 -0700

    added struct modifiers and struct functionHandlers

--- main.h
+++ main.h
@@ -15,6 +15,34 @@
 #define KILOBYTE 1024
 #define ABS(x) (((x) <  0) ? -(x) : (x))
 
+/**
+ * struct modifiers - struct containing flags for turning on
+ * passage of a flag to _printf()
+ * @plus: flag for the '+' character
+ * @space: flag for the ' ' character
+ * @hash: flag for the '#' character
+ */
+
+typedef struct modifiers
+{
+_Bool plus;
+_Bool space;
+_Bool hash;
+} modifs;
+
+/**
+ * struct printHandler - struct to choose the appropriate function
+ * subject to the format specifier passsed to _printf()
+ * @c: format specifier for character
+ * @f: pointer to printing function
+ */
+
+typedef struct functionHandler
+{
+char c;
+int (*f)(va_list ap, modifs *f);
+} fh;
+
 /* function prototypes */
 int _printf(const char *format, ...);
 int (*get_print(char s))(va_list, modifs *);
@@ -25,4 +53,5 @@ int _putchar(char c);
 int _puts(char *str);
 int print_percent(va_list l, modifs *f);
 _Bool invalidInputs(const char *p);
-#endif
+
+#endif /* end comment */
