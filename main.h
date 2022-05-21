407da20c08d4373962432c404a589bd2e8aacb05
Billy Otieno <billybrian94@gmail.com>
2022-05-21 23:37:57 +0300

    added defined macros

--- main.h
+++ main.h
@@ -4,6 +4,18 @@
 #include <stdarg.h>
 #include <assert.h>
 #include <stdbool.h>
+
+/* defined macros */
+#define FLUSH -1
+#define PF_INIT {false, false, false}
+#define NIL "(nil)"
+#define HEXA "0x"
+#define NULL_STRING "(null)"
+#define NUL '\0'
+#define KILOBYTE 1024
+#define ABS(x) (((x) <  0) ? -(x) : (x))
+
+/* function prototypes */
 int _printf(const char *format, ...);
 int (*get_print(char s))(va_list, modifs *);
 int get_flags(char s, modifs *f);
