c1bd81e525d371e9efb60befa9b1c638cc729f8b
Juliusbruce <brucejulius048@gmail.com>
2022-05-21 14:44:32 -0700

    print percent

---
+++ print_percent.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_percent - prints a percent
+ * @l: va_list arguments from _printf
+ * @f: pointer to the struct flags in which we turn the flags on
+ * Return: number of char printed
+ */
+int print_percent(va_list l, modifs *f)
+{
+(void)l;
+(void)f;
+return (_putchar('%'));
+}
