99278753735dba179e69ff3a5b53e12a51d7e678
billyotieno <billybrian94@gmail.com>
2022-05-21 14:38:04 -0700

    added logic to printf

--- _printf.c
+++ _printf.c
@@ -13,4 +13,29 @@ int _printf(const char *format, ...)
 	modifs prefixes = {false, false, false};
 	const char *p = format;
 	va_list arguments;
+
+/* checks for valid arguments */
+assert(invalidInputs(p));
+for (; *p; p++)
+{
+	if (*p == '%')
+	{
+		p++;
+		if (*p == '%')
+		{
+			len += _putchar('%');
+			continue;
+		}
+		while (get_flags(*p, &prefixes))
+		p++;
+		pfunction = fetch_print(*p);
+		len += (pfunction)
+		? pfunction(arguments, &prefixes)
+		: _printf("%%%c", *p);
+	} else
+	len += _putchar(*p);
+}
+_putchar(-1);
+va_end(arguments);
+return (len);
 }
