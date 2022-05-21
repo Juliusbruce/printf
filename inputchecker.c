7552c8782a5b705ab70593518cfa94797b457442
Juliusbruce <brucejulius048@gmail.com>
2022-05-21 14:15:58 -0700

    added input checker file

---
+++ inputchecker.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * invalidInputs - validates that the user did not enter invalid inputs
+ * @p: string of input
+ * Return: true or false
+ */
+ 
+_Bool invalidInputs(const char *p)
+{
+
+if (!p)
+return (false);
+
+if (*p == '%' && !*(p + 1))
+return (false);
+
+if (*p == '%' && *(p + 1) == ' ' && !*(p + 2))
+return (false);
+return (true);
+}
